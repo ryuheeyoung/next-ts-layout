4bce649245c6862244ed59bfc591c50ffd3ccef1
ryuheeyoung <rhy901008@gmail.com>
2022-01-27 16:03:34 +0900

    changing container2component

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,55 +1,11 @@
-import React from "react";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import { teal } from "@mui/material/colors";
-import { BoxProps, Color } from "@mui/material";
-import { TemplateLayout } from "../interfaces";
-import { useRouter } from "next/router";
 
-interface Colors extends Color {
-  [key: string]: string;
+interface LayoutProps {
+  children: React.ReactNode;
 }
 
-interface TypeProps {
-  layers: TemplateLayout[];
-}
-
-const Layout = ({ layers }: TypeProps) => {
-  return (
-    <Container
-      maxWidth={false}
-      sx={{
-        p: 0,
-        display: "flex",
-        width: "100vw",
-        height: "100vh",
-        m: 0,
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-      }}
-    >
-      {Array.from(Array(9).keys()).map((item, idx) => {
-        var cIndex = ((idx + 1) * 100).toString();
-        return (
-          <Box
-            key={cIndex}
-            sx={(theme) => ({
-              backgroundColor: (teal as Colors)[cIndex],
-              width: "25vw",
-              m: "1.25em",
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-            })}
-          >
-            <Box id={layers[idx]?.id.toString() || `item-${item + 1}`}>
-              {layers[idx]?.name ?? `Flex Box ${item + 1}`}
-            </Box>
-          </Box>
-        );
-      })}
-    </Container>
-  );
+const Layout = ({ children }: LayoutProps) => {
+  return <main>{children}</main>;
 };
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,66 @@
-import Layout from "../components/Layout";
-import { Layers } from "../utils/Layers";
+import { useRouter } from "next/router";
+import React, { useCallback } from "react";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import { teal } from "@mui/material/colors";
+import { Color, Link } from "@mui/material";
+
+import { TemplateLayout } from "../interfaces";
+import { Layers as layers } from "../utils/Layers";
+
+interface Colors extends Color {
+  [key: string]: string;
+}
 
 const IndexPage = () => {
-  return <Layout layers={Layers} />;
+  const router = useRouter();
+
+  const onClick = useCallback((layer: TemplateLayout) => {
+    router.push(layer.content);
+  }, []);
+  return (
+    <Container
+      maxWidth={false}
+      sx={{
+        p: 0,
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        m: 0,
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+      }}
+    >
+      {Array.from(Array(9).keys()).map((item, idx) => {
+        var cIndex = ((idx + 1) * 100).toString();
+        return (
+          <Box
+            key={layers[idx]?.id || cIndex}
+            sx={() => ({
+              backgroundColor: (teal as Colors)[cIndex],
+              width: "25vw",
+              m: "1.25em",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            })}
+          >
+            {layers[idx] ? (
+              <Link
+                underline="none"
+                color="inherit"
+                onClick={() => onClick(layers[idx])}
+              >
+                {layers[idx].name}
+              </Link>
+            ) : (
+              `Flex Box ${cIndex}`
+            )}
+          </Box>
+        );
+      })}
+    </Container>
+  );
 };
 export default IndexPage;
